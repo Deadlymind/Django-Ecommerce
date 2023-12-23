696316bd01cdddd74da23a1066fc27d8f4644e23
Deadlymind <oussamaayari2014@gmail.com>
2023-12-23 18:04:34 +0100

    show product images in product details

--- products/views.py
+++ products/views.py
@@ -17,4 +17,5 @@ class ProductDetail(DetailView):
         context = super().get_context_data(**kwargs)
         context["reviews"] = Review.objects.filter(product=self.get_object())
         context["images"] = ProductImage.objects.filter(product=self.get_object())
+        context['related'] = Product.objects.filter(brand=self.get_object().brand)
         return context
