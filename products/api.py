5b76de9d9db414b29e81e854dceae2c939a0a832
Deadlymind <oussamaayari2014@gmail.com>
2024-01-04 10:36:29 +0100

    add product & brand list api filter

--- products/api.py
+++ products/api.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+
 
 from .models import Product, Brand, Review, ProductImage
 from . import serializers
@@ -10,8 +12,9 @@ from .pagination import MyPagination
 class ProductListApi(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = serializers.ProductListSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['brand', 'flag']
+    search_fields = ['name', 'subtitle', 'description']
 
 
 class ProductDetailApi(generics.RetrieveAPIView):
@@ -24,6 +27,9 @@ class BrandListApi(generics.ListAPIView):
     queryset = Brand.objects.all()
     serializer_class = serializers.BrandListSerializer
     pagination_class = MyPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
+
 
 
 
