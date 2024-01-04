4455cbf518e374eeafb10f6161a9327d19af1949
Deadlymind <oussamaayari2014@gmail.com>
2024-01-04 17:07:16 +0100

    show review count in list & detail API

--- products/serializers.py
+++ products/serializers.py
@@ -3,16 +3,29 @@ from .models import Product, Brand
 
 class ProductListSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
+    review_count = serializers.SerializerMethodField()
     class Meta:
         model = Product
         fields = '__all__'
+
+    def get_review_count(self, object):
+        reviews = object.review_product.all().count()
+        
+        return reviews
 
 
 
 class ProductDetailSerializer(serializers.ModelSerializer):
+    brand = serializers.StringRelatedField()
+    review_count = serializers.SerializerMethodField()
     class Meta:
         model = Product
         fields = '__all__'
+
+    def get_review_count(self, object):
+        reviews = object.review_product.all().count()
+        
+        return reviews
 
 
 class BrandListSerializer(serializers.ModelSerializer):
