f10dbb29851f4de99071bd355348a77512ceb8ee
Deadlymind <oussamaayari2014@gmail.com>
2024-01-12 21:34:58 +0100

    add home template, clean navbar

--- products/serializers.py
+++ products/serializers.py
@@ -9,7 +9,6 @@ class ProductImageSerializer(serializers.ModelSerializer):
 
 class ProductListSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
-    review_count = serializers.SerializerMethodField()
     avg_rate = serializers.SerializerMethodField()
 
 
@@ -18,13 +17,7 @@ class ProductListSerializer(serializers.ModelSerializer):
         model = Product
         fields = '__all__'
 
-    def get_review_count(self, object):
-        reviews = object.review_count()
-        return reviews
-    
-    def get_avg_rate(self,object):
-        avg = object.avg_rate()
-        return avg
+
 
 
 
