e67577f7b9aa94176b96089200398398d5f42b96
Deadlymind <oussamaayari2014@gmail.com>
2024-01-12 22:04:15 +0100

    desing settings app models

--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+
+class Settings(models.Model):
+    name = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to='settings')
+    subtitle = models.TextField(max_length=500)
+    call_us = models.CharField(max_length=25)
+    email_us = models.EmailField(max_length=50, verbose_name=_('Email Us'))
+    email = models.EmailField(max_length=50)
+    phones = models.TextField(max_length=50)
+    address = models.TextField(max_length=100)
+    android_app = models.URLField(null=True, blank=True, verbose_name=_('Android App URL'))
+    ios_app = models.URLField(null=True, blank=True, verbose_name=_('iOS App URL'))
+    facebook = models.URLField(null=True, blank=True, verbose_name=_('Facebook URL'))
+    youtube = models.URLField(null=True, blank=True, verbose_name=_('YouTube URL'))
+
+    def __str__(self):
+        return self.name
