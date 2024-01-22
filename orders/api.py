450a1e82550cb1ac61712722aceb9e452580805d
Deadlymind <oussamaayari2014@gmail.com>
2024-01-22 04:54:33 +0100

    add cart API

--- orders/api.py
+++ orders/api.py
@@ -120,5 +120,41 @@ class CreateOrderAPI(generics.GenericAPIView):
 
 
 class CartCreateUpdateDelete(generics.GenericAPIView):
-    pass
+    
+    def get(self,request,*args, **kwargs):
+        # get_or_create
+        user = User.objects.get(username=self.kwargs['username'])
+        cart, created = Cart.objects.get_or_create(user=user,status='Inprogress')
+        data = CartSerializer(cart).data
+        return Response({'cart':data})
+    
+
+    def post(self,request,*args, **kwargs):
+        # add update
+        user = User.objects.get(username=self.kwargs['username'])
+        product = Product.objects.get(id=request.data['product_id'])
+        quantity = int(request.POST['quantity'])
+
+        cart = Cart.objects.get(user=user, status='Inprogress')
+        cart_detail, created = CartDetail.objects.get_or_create(cart=cart,product=product)
+
+        cart_detail.quantity = quantity
+        cart_detail.total = round(product.price * cart_detail.quantity,2)
+        cart_detail.save()
+
+        return Response({'message': 'cart was updated successfully'},status=status.HTTP_201_CREATED)
+
+
+    def delete(self,request,*args, **kwargs):
+        # delet from cart
+        user = User.objects.get(username=self.kwargs['username'])
+        # cart = Cart.objects,get(user=user,status='Inprogress')
+        product = CartDetail.objects.get(id=request.data['item_id'])
+        product.delete()
+
+        return Response({'message': 'item was deleted successfully'},status=status.HTTP_202_ACCEPTED)
+    
+
+
+
 
