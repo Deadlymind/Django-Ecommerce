8a7ba70831f55716dd56a8616160169efbbd18a6
Deadlymind <deadlygamex5@gmail.com>
2024-02-08 22:01:11 +0100

    test

--- accounts/models.py
+++ accounts/models.py
@@ -3,9 +3,16 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+PHONE_TYPE = (
+    ('Home','Home'),
+    ('Office','Office'),
+    
+)
+
 class ContactNumber(models.Model):
     user = models.ForeignKey(User, related_name='user_contact',on_delete=models.CASCADE)
     number = models.CharField(max_length=20)
+
 
 
 
@@ -20,4 +27,4 @@ class Address(models.Model):
     user = models.ForeignKey(User, related_name='user_address',on_delete=models.CASCADE)
     address = models.TextField(max_length=200)
     type = models.CharField(max_length=12,choices=ADDRESS_TYPE)
-    
+
