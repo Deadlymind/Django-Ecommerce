2c0342f4c7bd1c9ec5ff937ebaab61646003630b
Deadlymind <deadlygamex5@gmail.com>
2024-02-08 21:57:37 +0100

    update accounts

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class ContactNumber(models.Model):
+    user = models.ForeignKey(User, related_name='user_contact',on_delete=models.CASCADE)
+    number = models.CharField(max_length=20)
+
+
+
 ADDRESS_TYPE = (
     ('Home','Home'),
     ('Office','Office'),
